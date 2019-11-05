0072009ed33e97a559c56129df9d93869d8a6203
Your Name <you@example.com>
2019-11-05 16:26:59 +1100

    clean up

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,31 +1,39 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {updateCurrent, saveTodo} from '../reducers/todo'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { updateCurrent, saveTodo } from "../reducers/todo";
 
 class TodoForm extends Component {
-  handleInputChange = (evt) => {
-    const val = evt.target.value
-    this.props.updateCurrent(val)
-  }
+  handleInputChange = evt => {
+    const val = evt.target.value;
+    this.props.updateCurrent(val);
+  };
 
-  handleSubmit = (evt) => {
-    evt.preventDefault()
-    this.props.saveTodo(this.props.currentTodo)
-  }
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log("his.props.currentTodo",this.props.currentTodo)
+    if (this.props.currentTodo !== "") {
+      this.props.saveTodo(this.props.currentTodo);
+    } else {
+      alert("Please entry valid TODO ");
+    }
+  };
 
   render() {
-    const {currentTodo} = this.props
+    const { currentTodo } = this.props;
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text"
+        <input
+          type="text"
           onChange={this.handleInputChange}
-          value={currentTodo}/>
+          value={currentTodo}
+        />
+        <input type="submit" value="Add"></input>
       </form>
-    )
+    );
   }
 }
 
 export default connect(
-  (state) => ({currentTodo: state.todo.currentTodo}),
-  {updateCurrent, saveTodo}
-)(TodoForm)
+  state => ({ currentTodo: state.todo.currentTodo }),
+  { updateCurrent, saveTodo }
+)(TodoForm);
